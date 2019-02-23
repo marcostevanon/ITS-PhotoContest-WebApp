5cbd6f2fc8f177ab4898ee958474ee57640d1332
Marco Stevanon <marco.stevanon@gmail.com>
2019-02-23 20:23:35 +0100

    add user constructor

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,8 +1,19 @@
 export class User {
     id: number
     username: string
+    password: string
     email: string
     firstname: string
     lastname: string
     avatar: string
+
+    constructor(firstname, lastname, email, username, password, avatar) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.avatar = avatar;
+    }
+
 }
