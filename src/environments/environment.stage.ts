d41a6dd08b64817925b20dcd481c30a8d8ba42e5
Marco Stevanon <marco.stevanon@gmail.com>
2019-03-13 11:57:24 +0100

    fix stage env api_endpoint

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  api_endpoint: 'http://steva01:5671' + '/api',
+  api_endpoint: 'http://199.247.4.153:5671' + '/api',
   default_avatar: 'https://d3v4yqdep5qmmr.cloudfront.net/images/default/avatar.jpg'
 };
