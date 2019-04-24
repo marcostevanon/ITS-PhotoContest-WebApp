13b1d9ac16758048c0e5be0991454335beebeb4a
Marco Stevanon <marco.stevanon@gmail.com>
2019-04-24 21:57:31 +0200

    fix envs

--- src/environments/environment.vultr.ts
+++ src/environments/environment.vultr.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  api_endpoint: 'https://api.photocontest.marcostevanon.ovh/',
+  production: true,
+  api_endpoint: 'https://api.photocontest.marcostevanon.ovh',
   default_avatar: 'https://d3v4yqdep5qmmr.cloudfront.net/images/default/avatar.jpg'
 };
